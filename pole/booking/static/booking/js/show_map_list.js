8a210ca29fb0203f794acfea67923b83375046cd
abdullokhakimov <abdullohxakimov7@gmail.com>
2023-07-11 15:29:36 +0500

    new

--- pole/booking/static/booking/js/show_map_list.js
+++ pole/booking/static/booking/js/show_map_list.js
@@ -1,9 +1,7 @@
 const showMapListBtn = document.querySelector('.show__cart-list');
 const showMapListBtnText = document.querySelector('.show__cart-list span');
-const header = document.querySelector('.header');
 const myMap = document.querySelector('.map');
 const mainContent = document.querySelector('.main-content');
-const myHTML = document.querySelector('html');
 
 function showMapListToggle() {
     showMapListBtn.classList.toggle("list");
@@ -15,10 +13,6 @@ function showMapListToggle() {
 		showMapListBtnText.innerHTML = 'Показать список';
 	}
 }
-
-const screenHeight = screen.height;
-const screenWidth = screen.width;
-const headerHeight = screenHeight - 280;
 
 if (screenWidth < 576) {
 	header.style.height = `${headerHeight}px`;
